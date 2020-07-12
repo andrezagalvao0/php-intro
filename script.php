6b8a4274be818bea7d1e746f97608500e6f2d040
andreza <andreza.galvao.al@gmail.com>
2020-07-12 01:23:30 -0300

    atualizaçao de validacao de formulario

--- script.php
+++ script.php
@@ -6,6 +6,26 @@ $categoria = ['infantil', 'adolescente', 'adulto', 'idoso'];
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
+if(empty($nome)){
+    echo 'Nome nao pode ser vazio';
+    return;
+}
+
+if(strlen($nome) < 3){
+    echo 'O nome nao pode ter menos que 3 caracteres';
+    return;
+}
+
+if(strlen($nome) > 40){
+    echo 'O nome nao pode ter mais que 40 caracteres';
+    return;
+}
+
+if(!is_numeric($idade)){
+    echo 'So eh permitido valores numericos';
+    return;
+}
+
 if($idade >= 6  && $idade <= 12)
 {
     echo("A nadadora " .$nome. " compete na categoria infantil");
