de52c78f30d72da7ffccc4851c44a0f8c6d72e40
andreza <andreza.galvao.al@gmail.com>
2020-07-12 04:18:05 -0300

    vaidaçao dos dados da sessao

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+    session_start();
+?>
+
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -9,6 +14,14 @@
         <p>FORMULARIO PARA A INCRIÇAO DE COMPETIDORES</p>
 
         <form action="script.php" method="post">
+            <?php
+                $mensagemDeErro = isset($_SESSION['mensagem-de-erro']) ? $_SESSION['mensagem-de-erro'] : ' ' ;// a ? seria o if, caso seja true//se minha sessao na chave 'mensagem de erro' tiver um valor iniciado eu quero mostrar ele
+                if(!empty($mensagemDeErro))
+                {
+                    echo $mensagemDeErro;
+                }
+            ?>
+
             <p>Seu nome: <input type="text" name="nome"/></p>
             <p>Sua idade: <input type="text" name="idade"/></p>
             <p><input type="submit" value="Enviar dados do competidor"/> </p>
--- script.php
+++ script.php
@@ -1,40 +1,54 @@
 <?php
 
-$categoria = ['infantil', 'adolescente', 'adulto', 'idoso'];
+session_start();
+
+$categoria = ['infantil', 'adolescente', 'adulto', 'idoso']; //array numerico
 
 
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
 if(empty($nome)){
-    echo 'Nome nao pode ser vazio';
-    return;
+    $_SESSION['mensagem-de-erro'] = 'Nome nao pode ser vazio, por favor preencha'; //$SESSION[''] array associativo(associar um valor a uma chave, que por sua vez nao necessariamente sera numerica)
+    header('location: index.php'); //isso faz com que a mensagem de erro seja exibida na mesma pagina(se der algum tipo de erro ele vai redirecionar para a pagina index.php)
+
 }
 
 if(strlen($nome) < 3){
-    echo 'O nome nao pode ter menos que 3 caracteres';
-    return;
+    $_SESSION['mensagem-de-erro'] = 'O nome nao pode ter menos que 3 caracteres';
+    header('location: index.php');
 }
 
 if(strlen($nome) > 40){
-    echo 'O nome nao pode ter mais que 40 caracteres';
-    return;
+    $_SESSION['mensagem-de-erro'] = 'O nome nao pode ter mais que 40 caracteres';
+    header('location: index.php');
 }
 
 if(!is_numeric($idade)){
-    echo 'So eh permitido valores numericos';
-    return;
+    $_SESSION['mensagem-de-erro'] = 'So eh permitido valores numericos';
+    header('location: index.php');
+    
 }
 
 if($idade >= 6  && $idade <= 12)
 {
-    echo("A nadadora " .$nome. " compete na categoria infantil");
+    for ($i =0; $i <= count($categoria); $i++){
+        if($categoria[$i] == 'infantil')
+        echo "O nadador " .$nome. " compete na categoria " .$categoria[$i];
+    }
+    
 } else if($idade >= 13 && $idade <= 18)
 {
-    echo("A nadadora " .$nome. " compete na categoria adolescente");
+    for ($i =0; $i <= count($categoria); $i++)   
+    if($categoria[$i] == 'adolescente')
+    echo("A nadadora " .$nome. " compete na categoria " .$categoria[$i]);
+
 }else if($idade >= 19 && $idade <= 50)
 {
-    echo("A nadadora " .$nome. " compete na categoria adulto");
+    for ($i =0; $i <= count($categoria); $i++)   
+    if($categoria[$i] == 'adulto')
+    echo("A nadadora " .$nome. " compete na categoria " .$categoria[$i]);
+
 } else{
     echo("A nadadora " .$nome. " compete na categoria idoso");
 }
